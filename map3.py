13aa359a6ce8f8ef3a927fae658402827f8c102d
ynd24 <yuvraj.nihal.duggal@emory.edu>
2022-07-05 10:34:20 +0530

    Update1

--- map3.py
+++ map3.py
@@ -12,11 +12,11 @@ map = folium.Map(location=[41.089742,-118.420348], tile="Stamen", zoom_start=2)
  
 fgp = folium.FeatureGroup(name = "Wrold Population")    
 fgp.add_child(folium.features.GeoJson(data=data1,  
-style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 20000000 else 'yellow' 
-if 20000000 <= x['properties']['POP2005'] < 50000000 else 'orange' if 50000000 <= x['properties']['POP2005'] < 100000000
-else 'red'}, tooltip = folium.features.GeoJsonTooltip(fields=['NAME','AREA','POP2005'],
-aliases=['NAME:','AREA:','POPULATION:'],
-style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"))))
+    style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 20000000 else 'yellow' 
+        if 20000000 <= x['properties']['POP2005'] < 50000000 else 'orange' if 50000000 <= x['properties']['POP2005'] < 100000000
+            else 'red'}, tooltip = folium.features.GeoJsonTooltip(fields=['NAME','AREA','POP2005', 'LON', 'LAT'],
+                aliases=['NAME:','AREA:','POPULATION:', 'LONGITUDE:', 'LATITUDE:'],
+                    style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"))))
  
  
  
